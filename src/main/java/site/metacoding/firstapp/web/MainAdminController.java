93eda7554a3e33fc138de12808c0592cdf77bbe2
mysjw1028 <108706248+mysjw1028@users.noreply.github.com>
2022-11-17 21:05:03 +0900

    중앙관리자 헤더 / 로그인 부분 수정 완료

--- src/main/java/site/metacoding/firstapp/web/MainAdminController.java
+++ src/main/java/site/metacoding/firstapp/web/MainAdminController.java
@@ -32,18 +32,18 @@ public class MainAdminController {
 	public String 중앙관리자회원가입(MainAdmin mainAdmin) {// 로그인시 중앙관리자 패스워드 번호를 if 문 돌려서 4567아니면 구매자 / 일반관리자 페이지로 이동
 		System.out.println(mainAdmin.getMainadminName());
 		System.out.println(mainAdmin.getPasswordMainadmin());
-		if (mainAdmin.getPasswordMainadmin().equals("5678")) {// 회원가입시 PasswordMainadmin에 4567울
+		if (mainAdmin.getPasswordMainadmin().equals("5678")) {
 			System.out.println(mainAdmin.getPasswordMainadmin());
 			mainAdminDao.insert(mainAdmin);
 			return "mainadmin/mainadminlogin";
 		} else {
 			return "mainadmin/mainadminjoin";
 		}
-	}
+	}// 회원가입시 PasswordMainadmin에 5678을 넣지 않으면 가입불가
 
 	@GetMapping("/mainadmin/loginpage")
 	public String mainadminlogin() {// 주소창 입력시 화면에 출력
-		return "mainadmin/mainadminlogin";// 로그인시 중앙관리자 패스워드 번호를 if 문 돌려서 4567아니면 구매자 / 일반관리자 페이지로 이동
+		return "mainadmin/mainadminlogin";
 	}
 
 	@PostMapping("/Mainadmin/loginpage")
@@ -53,17 +53,13 @@ public class MainAdminController {
 		if (loginRespDto != null) {
 			return "redirect:/";
 		}
-		return "mainadmin/mainadminlogin";// 인증실패하면
-	}
-//		if (mainAdmin.getPasswordMainadmin() != "4567") {
-//		return "redirect:/";// model를 사용 했을때 "redirect:/" 이거를 사용해야 화면이 보여진다.
-//	} else
-//어차피 가입할때 중앙관리자 번호 입력해서 로그인하는데 의미 가잇나
-	// 로그인시 중앙관리자 패스워드 번호를 if 문 돌려서 4567아니면 구매자 / 일반관리자 페이지로 이동
+		return "mainadmin/mainadminlogin";// 인증실패하면 다시 로그인해야함
+	}// 회원가입하면서 중앙관리자 체크함
 
 	@GetMapping("/Mainadmin/userlist")
-	public String userlist(@PathVariable Integer userId, Model model) {// 주소창 입력시 화면에 출력
-		model.addAttribute("users", usersDao.findById(userId));
+	public String userlist() {// 주소창 입력시 화면에 출력
+		//@PathVariable Integer userId, Model model
+//		model.addAttribute("users", usersDao.findById(userId));
 		return "mainadmin/userlist";
 
 	}
--- src/main/webapp/WEB-INF/views/users/login.jsp
+++ src/main/webapp/WEB-INF/views/users/login.jsp
@@ -2,6 +2,7 @@
 
 <%@ include file="../layout/header.jsp"%>
 
+
 <div class="container">
 	<form action="/login" method="post">
 		<div class="mb-3 mt-3">
@@ -13,6 +14,8 @@
 		<button type="submit" class="btn btn-primary">개인 로그인</button>
 		<button type="submit" class="btn btn-primary" onclick="javascript: form.action='/admin/login';">관리자
 			로그인</button>
-</div
+		<a class="btn btn-primary" href="/mainadmin/loginpage">중앙 관리자 로그인 페이지로 이동</a>
+</div>
+
 
 <%@ include file="../layout/footer.jsp"%>
