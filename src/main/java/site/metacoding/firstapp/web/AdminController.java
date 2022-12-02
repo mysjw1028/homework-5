4d20d407a4dcde0b8264bc542218cc7943ccf7d2
mysjw1028 <108706248+mysjw1028@users.noreply.github.com>
2022-12-02 09:45:41 +0900

    admin회원가입 JoinDto null 값체크 if문 완료

--- src/main/java/site/metacoding/firstapp/web/AdminController.java
+++ src/main/java/site/metacoding/firstapp/web/AdminController.java
@@ -35,7 +35,19 @@ public class AdminController {
 	public @ResponseBody CMRespDto<?> 관리자회원가입(@RequestBody AdminJoinDto adminJoinDto) {
 		// 테스트할때 디비 / Dao /Dto 확인 후 값이 안들어가면 view name이 엔티티랑 같은지 확인하기!
 		adminService.관리자회원가입(adminJoinDto);
-		return new CMRespDto<>(1, "일반 회원가입 성공", null);
+		if (adminJoinDto.getAdminName() == null && adminJoinDto.equals(" ")) {
+			return new CMRespDto<>(-1, "관리자회원가입 실패", null);
+		} else if (adminJoinDto.getPassword() == null && adminJoinDto.equals(" ")) {
+			return new CMRespDto<>(-1, "관리자회원가입 실패", null);
+		} else if (adminJoinDto.getEmail() == null && adminJoinDto.equals(" ")) {
+			return new CMRespDto<>(-1, "관리자회원가입 실패", null);
+		}
+		System.out.println("==============성공시======================");
+		System.out.println(adminJoinDto.getAdminName());
+		System.out.println(adminJoinDto.getPassword());
+		System.out.println(adminJoinDto.getEmail());
+		System.out.println("==============성공시======================");
+		return new CMRespDto<>(1, "관리자회원가입 성공", null);
 	}// 디비에 값 들어가는거 확인 완료
 
 	@GetMapping("/admin/login") // 화면 출력되는지 확인 완료
@@ -54,7 +66,7 @@ public class AdminController {
 	public @ResponseBody CMRespDto<?> 관리자로그인(@RequestBody AdminLoginDto adminLoginDto, HttpServletResponse response) {// 로그인
 
 		if (adminLoginDto.isRemember()) {
-			Cookie cookie = new Cookie("adminName", adminLoginDto.getAdminName()); 
+			Cookie cookie = new Cookie("adminName", adminLoginDto.getAdminName());
 			cookie.setMaxAge(60 * 60 * 24);
 			response.addCookie(cookie);
 		} else {
@@ -68,6 +80,12 @@ public class AdminController {
 		}
 		session.setAttribute("principal", principal);
 		return new CMRespDto<>(1, "로그인성공", null);
+	}
+
+	@GetMapping("/admin/join/adminNameCheck")
+	public @ResponseBody CMRespDto<Boolean> adminameSameCheck(String adminName) {
+		boolean isSame = adminService.관리자중복체크(adminName);
+		return new CMRespDto<>(1, "성공", isSame);
 	}
 
 }
--- src/main/webapp/WEB-INF/views/admin/adminjoin.jsp
+++ src/main/webapp/WEB-INF/views/admin/adminjoin.jsp
@@ -1,6 +1,6 @@
 ﻿<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-<%@ include file="../layout/header.jsp"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ include file="../layout/header.jsp"%>
 
 <div class="container">
 	<form>
